6908ebdd707ff378b62efc23f192cb9eaf61d0ef
Sebastian Fals <77508405+Sebastian-Fals@users.noreply.github.com>
2024-06-03 15:35:13 -0500

    portfolio ready

--- src/app/components/sobre-mi/sobre-mi.component.ts
+++ src/app/components/sobre-mi/sobre-mi.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {
+  faPython,
+  faHtml5,
+  faCss3,
+  faJs,
+  faNodeJs,
+  faAngular,
+  faReact,
+} from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-sobre-mi',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './sobre-mi.component.html',
-  styleUrl: './sobre-mi.component.scss'
+  styleUrl: './sobre-mi.component.scss',
 })
 export class SobreMiComponent {
-
+  python = faPython;
+  html = faHtml5;
+  css = faCss3;
+  js = faJs;
+  node = faNodeJs;
+  angular = faAngular;
+  react = faReact;
 }
